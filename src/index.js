984aec675070423329023dd9fdb999b019e82b9c
scake429 <scake429@gmail.com>
2023-10-24 15:23:29 +0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+module.exports = function reverse (n) {
+    console.log(Number(Math.abs(n).toString().split('').reverse().join('')));
+    return Number(Math.abs(n).toString().split('').reverse().join(''));
+  
+}
